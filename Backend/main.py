9b12ef9b61dea54cc2a58ad494d1562b64ea3d3a
Vishakha Gupta <vgupta0502@gmail.com>
2026-04-12 01:21:02 +0530

    Completed booking, payment, history, admin CRUD APIs with fixes and allows React frontend

--- Backend/main.py
+++ Backend/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from database import Base, engine
 import models
 from routers import user,hotels,rooms,bookings, admin
+from fastapi.middleware.cors import CORSMiddleware 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # allow React frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 Base.metadata.create_all(bind=engine)   # Creates tables if they don't exist
 
